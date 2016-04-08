f62106ef0fca2937e7bacefeffb5fd3c96500bd1
ACharLuk <acharluk@gmail.com>
2016-04-08 23:22:01 +0200

    Added Random functionality

---
+++ src/math/Random.h
@@ -0,0 +1,8 @@
+#ifndef __PPE_RANDOM__
+#define __PPE_RANDOM__
+
+#include <random>
+namespace ppe_math {
+	int GetRandomInt(int lowest, int highest);
+}
+#endif
--- test/Test.cpp
+++ test/Test.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "../src/math/Random.h"
 
 using std::cout;
 using std::endl;
 
 int main() {
 	cout << "Hello PPE!" << endl;
+
+	for (int i = 0; i < 10; ++i) {
+		cout << ppe_math::GetRandomInt(0, 50) << endl;
+	}
 }
