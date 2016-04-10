763f0605923acad9d7ea9fa68969f4258c66c54c
ACharLuk <acharluk@gmail.com>
2016-04-10 12:15:53 +0200

    Reading every region, fixed caps

--- src/PPE.cpp
+++ src/PPE.cpp
@@ -8,13 +8,22 @@ using ppe_core::management::PokemonManager;
 PpeEngine::PpeEngine() {
 	PokemonManager pkmanager;
 	//pkmanager.RegisterPokemons("resources/pokemon_data.ppe");
-	pkmanager.RegisterPokemons("resources/kanto.ppe");
+
+	pkmanager.RegisterPokemons("resources/Kanto.ppe");
 	std::cout << std::endl;
-	//pkmanager.RegisterPokemons("resources/Johto.ppe");
+
+	pkmanager.RegisterPokemons("resources/Johto.ppe");
 	std::cout << std::endl;
-	//pkmanager.RegisterPokemons("resources/Teselia.ppe");
+
+	pkmanager.RegisterPokemons("resources/Hoenn.ppe");
 	std::cout << std::endl;
-	//pkmanager.RegisterPokemons("resources/Kalos.ppe");
+
+	pkmanager.RegisterPokemons("resources/Teselia.ppe");
+	std::cout << std::endl;
+
+	pkmanager.RegisterPokemons("resources/Kalos.ppe");
+	std::cout << std::endl;
+	
 }
 
 PpeEngine::~PpeEngine() {
